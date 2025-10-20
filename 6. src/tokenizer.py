5ab86e49985b65ea995cdf904b5db1c925ebef24
Hakuna <98fenwickt@gmail.com>
2025-10-20 08:45:27 -0400

    Create tokenizer utility script
    Added a script for loading tokenizers using transformers.

--- 6. src/tokenizer.py
+++ 6. src/tokenizer.py
@@ -1,1 +1,6 @@
-
+  - Create a script for tokenizer utilities:
+     ```python
+     from transformers import AutoTokenizer
+     def load_tokenizer(model_path):
+         return AutoTokenizer.from_pretrained(model_path)
+     ```
